e29fb0f7a987d861a61ac65b95be90d12a1266cf
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-09 01:13:50 -0700

    add and implement updateTimeFn() to make the timer update every second automatically;

--- simple-swift-project/ViewController.swift
+++ simple-swift-project/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet var checkTime : UILabel
     let dateFormatter = NSDateFormatter()
     let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
-    
+
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+       var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updateTimerFn"), userInfo: nil, repeats: true)
 
+        dateFormatter.dateFormat = "h:mm:ss a"
+        
+        
         button.frame = CGRectMake(25, 25, 250, 25)
         button.setTitle("Say \"Hello World!\" and display time", forState: UIControlState.Normal)
         button.addTarget(self, action: "callback_01:", forControlEvents: UIControlEvents.TouchUpInside)
@@ -34,11 +38,14 @@ class ViewController: UIViewController {
 
     func callback_01(sender: UIButton!) {
         
-        dateFormatter.dateFormat = "h:mm:ss a"
         var date = NSDate()
         
         checkTime.text = "Hello World! " + dateFormatter.stringFromDate(date)
         println("Hello World! " + dateFormatter.stringFromDate(date))
+    }
+    
+    func updateTimerFn() {
+        checkTime.text = dateFormatter.stringFromDate(NSDate())
     }
 }
 
