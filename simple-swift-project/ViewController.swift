ba0ed3ea0d41a367e4d60bcdcd730b4833314b01
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-08 21:24:29 -0700

    (1) add an UIButton and its callback; (2) add label to display the message from the UIButton being clicked;

--- simple-swift-project/ViewController.swift
+++ simple-swift-project/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+
+    @IBOutlet var checkTime : UILabel
+    let dateFormatter = NSDateFormatter()
+    let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        button.frame = CGRectMake(25, 25, 250, 25)
+        button.setTitle("Say \"Hello World!\" and display time", forState: UIControlState.Normal)
+        button.addTarget(self, action: "callback_01:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(button)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func callback_01(sender: UIButton!) {
+        
+        dateFormatter.dateFormat = "h:mm:ss a"
+        var date = NSDate()
+        
+        checkTime.text = "Hello World! " + dateFormatter.stringFromDate(date)
+        println("Hello World! " + dateFormatter.stringFromDate(date))
+    }
 }
 
