e7cfeaa6bb19e41dabb7728ad6ce2ac8146f63c0
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-11 23:30:09 -0700

    add NewTaskViewController;

---
+++ simple-swift-project/NewTaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewTaskViewController.swift
+//  simple-swift-project
+//
+//  Created by ctrl-alt-del on 6/11/14.
+//  Copyright (c) 2014 ctrl-alt-del. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
